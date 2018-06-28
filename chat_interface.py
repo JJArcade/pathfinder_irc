45f8733ed06954db425c8d809c0bdf6919f161c3
JJ <jjuarez@czarnowski.com>
2018-06-28 16:25:55 -0400

    added stats command

--- chat_interface.py
+++ chat_interface.py
@@ -1,1 +1,8 @@
-import sopel.module
+from sopel import module
+from roleplay import roleplay
+
+rp = roleplay()
+@module.commands("char_info", "info", "stats")
+def printChar(bot, trigger):
+    out = rp.printCharInfo(trigger.group(2))
+    bot.reply(out)
